4b4d21ca608ff52cbb6e16635e9f91097918a319
FelipeGodoy1 <godoy347@hotmail.com>
2022-05-16 22:31:43 -0300

    Criação do WinForms de Cadastro de Peças.

---
+++ SistemaOrdemServico/Pecas.Designer.cs
@@ -0,0 +1,102 @@
+﻿
+namespace SistemaOrdemServico
+{
+    partial class Pecas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControlPecas = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabControlPecas.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControlPecas
+            // 
+            this.tabControlPecas.Controls.Add(this.tabPage1);
+            this.tabControlPecas.Controls.Add(this.tabPage2);
+            this.tabControlPecas.Controls.Add(this.tabPage3);
+            this.tabControlPecas.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControlPecas.Location = new System.Drawing.Point(0, 0);
+            this.tabControlPecas.Name = "tabControlPecas";
+            this.tabControlPecas.SelectedIndex = 0;
+            this.tabControlPecas.Size = new System.Drawing.Size(979, 541);
+            this.tabControlPecas.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(971, 515);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(971, 515);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(971, 515);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "tabPage3";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // Pecas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(979, 541);
+            this.Controls.Add(this.tabControlPecas);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Pecas";
+            this.ShowIcon = false;
+            this.Text = "Pecas";
+            this.tabControlPecas.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControlPecas;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+    }
+}
