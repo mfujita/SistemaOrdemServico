54333103cbf976a5c97567fa394a67373f3a3b0b
FelipeGodoy1 <godoy347@hotmail.com>
2022-05-23 20:34:50 -0300

    Validacoes de alguns campos de cadstro pecas

--- SistemaOrdemServico/CadastroPecas.cs
+++ SistemaOrdemServico/CadastroPecas.cs
@@ -25,10 +25,10 @@ namespace SistemaOrdemServico
             //Declarações das variaveis do input text
 
             string cadastroNomePeca = txtNomeCadastrarPeca.Text;
-            string cadastroFornecedorPeca = txtFornecedorCadastrarPeca.Text;
+            string cadastroFornecedorPeca = cboxFornecedorCadastrarPeca.Text;
             string cadastroFabricantePeca = txtFabricanteCadastrarPeca.Text;
-            string cadastroValorCompra = txtValorCompraCadastrarPeca.Text;
-            string cadastroValorVenda = txtValorVendaCadastrarPeca.Text;
+            string cadastroValorCompra = numericValorCompraCadastrarPeca.Text;
+            string cadastroValorVenda = numericValorVendaCadastrarPeca.Text;
 
             //Validaçao do formulario 
             if( cadastroNomePeca == string.Empty || 
@@ -43,15 +43,75 @@ namespace SistemaOrdemServico
             {
                 //Area do banco de dados
 
-                /*string sql = "INSERT INTO  nomeTabela VALUES ( '"+cadastroNomePeca+"'," +
+                string sql = "INSERT INTO  nomeTabela VALUES ( '"+cadastroNomePeca+"'," +
                     "'"+cadastroFornecedorPeca+"'," +
                     "'"+cadastroFabricantePeca+"'," +
-                    ""+Convert.ToDouble(cadastroValorCompra)+"," +
-                    ""+Convert.ToDouble(cadastroValorVenda)+")";*/
+                    ""+Convert.ToDouble(cadastroValorCompra.Replace(",","."))+"," +
+                    ""+Convert.ToDouble(cadastroValorVenda.Replace(",","."))+")";
 
             }
 
 
         }
+
+
+        //Area de deletar a peça
+        private void buttonDeletarPeca_Click(object sender, EventArgs e)
+        {
+            string idDelete = txtIdDeletarPeca.Text;
+
+            if ( idDelete == string.Empty )
+            {
+                MessageBox.Show("Insira o Id da peça para deletar");
+            }
+            else
+            {
+                //string sql = "DELETE FROM 'cadastropeca' WHERE ID = " + Convert.ToInt32( idDelete) + "";
+
+                /*SqlCommand comando = new SqlCommand(sql, conexao);
+
+                conexao.Open();
+                comando.ExecuteNonQuery();
+
+                comando.Dispose();
+                conexao.Close();*/
+            }
+        }
+
+
+
+
+        //Botao de consulta
+        private void btnConsultar_Click(object sender, EventArgs e)
+        {
+            listagem();
+            btnConsultar.Enabled = false;
+
+ 
+        }
+
+        //Listagem para dar select
+        private void listagem()
+        {
+
+
+            string sql = "SELECT * FROM 'nome da tabela'";
+
+            /*SqlCommand comando = new SqlCommand(sql, conexao);
+
+            conexao.Open();
+            SqlDataReader dados = comando.ExecuteReader();
+
+            comando.Dispose();
+
+            while (dados.Read())
+            {
+                dgvPecas.Rows.Add(dados[0], dados[1], dados[2], dados[3], dados[4], dados[5]);
+            }
+
+            conexao.Close();*/
+
+        }
+
     }
 }
