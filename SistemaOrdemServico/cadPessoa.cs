ac772dd7911e15074f8713bdb9190d8bc1f96b47
Lucas Burim <lburim65@gmail.com>
2022-05-23 22:33:56 -0300

    Alterações dia 23/05/2022 tela cadPessoa

--- SistemaOrdemServico/cadPessoa.cs
+++ SistemaOrdemServico/cadPessoa.cs
@@ -8,23 +8,24 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+//Form CadPessoas do burim
+//***NÃO MEXER! SUJEITO A CACETE***
+
 namespace SistemaOrdemServico
 {
     public partial class cadPessoa : Form
     {
-        Form1 frm = new Form1();
         public cadPessoa()
         {
             InitializeComponent();
         }
-        //Butão SALVAR do burim ***NÃO MEXER! SUJEITO A CACETE***
         private void btnSalvarPessoa_Click(object sender, EventArgs e)
         {
-            //Get de todos os campos da tela de cadastro de cliente
+            //Get de todos os campos da tela de cadastro de cliente e verifica se estão em branco
             Dictionary<string, string> validaCamposCadClie = new Dictionary<string, string>
             {
                 {lblNomePessoa.Text     , txtNomePessoa.Text},
-                {lblCpfCnpj.Text        , txtCpfCnpj.Text},
+                {lblCpfCnpj.Text        , txtCpfCnpj.Text == },//ta pegando a máscara dos campos
                 {lblCepPessoa.Text      , txtCepPessoa.Text},
                 {lblEstadoPessoa.Text   , txtEstadoPessoa.Text},
                 {lblCidadePessoa.Text   , txtCidadePessoa.Text},
@@ -39,27 +40,24 @@ namespace SistemaOrdemServico
 
             if (!Form1.TemCamposVazios(validaCamposCadClie))
             {
-                salvaCampos();
+                getDados();
                 MessageBox.Show("OK", "Cadastro realizado com sucesso");
             }
         }
 
-        //Butão SALVAR do burim ***NÃO MEXER! SUJEITO A CACETE***
         private void btnEditarPessoa_Click(object sender, EventArgs e)
         {
-            //verificaCampos();
+
         }
-        //Butão SALVAR do burim ***NÃO MEXER! SUJEITO A CACETE***
         private void btnExcluirPessoa_Click(object sender, EventArgs e)
         {
 
         }
-        //Salva os campos puxar classe conecta banco e passar como parâmetros os valores dessa classe
-        //***NÃO MEXER! SUJEITO A CACETE***
-        public void salvaCampos()
+        //coleta todos os dados e passa em variáveis
+        public void getDados()
         {
-            int cpfCnpj = Convert.ToInt32(txtCpfCnpj);
-            int cep = Convert.ToInt32(txtCepPessoa.Text);
+            string cpfCnpj = txtCpfCnpj.Text;
+            string cep = txtCepPessoa.Text;
             string nomePessoa = txtNomePessoa.Text;
             string estado = txtEstadoPessoa.Text;
             string cidade = txtCidadePessoa.Text;
@@ -70,6 +68,18 @@ namespace SistemaOrdemServico
             string email = txtEmailPessoa.Text;
             string status = cbStatusPessoa.Text;
             string categoria = cbCategoriaPessoa.Text;
+        }
+
+        public void insertCadPess(){
+
+        }
+        public void updateCadPess()
+        {
+
+        }
+        public void deleteCadPess()
+        {
+
         }
     }
 }
