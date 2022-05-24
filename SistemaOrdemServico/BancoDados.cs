cb7326fc27b3ed2436d713f976f601015ef363cb
FelipeGodoy1 <godoy347@hotmail.com>
2022-05-24 07:46:55 -0300

    Criação da classe que conecta o banco de dados.

---
+++ SistemaOrdemServico/BancoDados.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaOrdemServico
+{
+    class BancoDados
+    {
+        public SqlConnection AbreConexao()
+        {
+            string conexao = @"'NOME-DO-SERVIDOR-DA-MAQUINA';
+                                   Database=OSFujita;
+                                   User Id=sa; 
+                                   Password=1234;";
+            return new SqlConnection(conexao);
+        }
+    }
+}
